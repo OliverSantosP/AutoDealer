aeca55b16b017d5e3502f2cf4c9aedcb039b51a2
Oliver Santos Piña <oliversantosp@gmail.com>
2014-01-08 00:06:37 -0400

    Modificada ruta standard
    Ahora la primera pagina sera la accion Index de Automoviles.

--- AutoDealer/App_Start/RouteConfig.cs
+++ AutoDealer/App_Start/RouteConfig.cs
@@ -42,7 +42,7 @@ namespace AutoDealer
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Automovil", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
